d7e8e82f5fcc04afc5fd6ad5ab778ff19448c3ce
Rahaf Alenezi <irahaf@live.it>
2022-06-23 16:31:32 +0300

    Fixed modal, still you have some missing functions

--- trips/components/TripAddModal.js
+++ trips/components/TripAddModal.js
@@ -1,9 +1,11 @@
 // Formik x React Native example
 import React from "react";
 import { Button, TextInput, View } from "react-native";
-import { Formik } from "formik";
+import { Formik } from "formik"; // ! did you download it?
+import { Input } from "@rneui/themed-edge";
 
-export const TripAddModal = (props) => (
+// ! you forgot to default export it and you're default importing it from App.js, we're too old for this mistake
+const TripAddModal = (props) => (
   <Formik
     initialValues={{ trip: "" }}
     onSubmit={(values) => console.log(values)}
@@ -11,8 +13,14 @@ export const TripAddModal = (props) => (
     {({ handleChange, handleBlur, handleSubmit, values }) => (
       <View>
         <TextInput
-          onChangeText={handleChange("Trip")}
-          onBlur={handleBlur("Trip")}
+          onChangeText={handleChange} // ! if you want to call a function in JSX as follows: {whatever()} you have to put it in an anonymous function {()=>whatever()}
+          onBlur={handleBlur}
+          value={values.trip}
+        />
+        <Input
+          placeholder="Trip Name"
+          onChangeText={handleChange}
+          onBlur={handleBlur}
           value={values.trip}
         />
         <Button onPress={handleSubmit} title="Create" />
@@ -21,3 +29,5 @@ export const TripAddModal = (props) => (
     )}
   </Formik>
 );
+
+export default TripAddModal;
